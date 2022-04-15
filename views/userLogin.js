b35b1128689836e54c3fdd6312dd7c19181e80eb
Jan <moralesjan40@gmail.com>
2022-04-15 11:12:12 -0400

    commiting auth changes

--- views/userLogin.js
+++ views/userLogin.js
@@ -10,6 +10,7 @@ fetch(baseUrl, {
 
 const signUpButton = document.getElementById('signUp');
 const signUpForm = document.getElementById('sign-up-form')
+const signInForm = document.getElementById('sign-in-form')
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
@@ -26,6 +27,7 @@ signUpForm.addEventListener('submit', async function(event){
     const name = data.get("name");
     const email = data.get("email");
     const password = data.get("password");
+    console.log(name, email, password)
   
     const response = await fetch(`${baseUrl}/signup`, {
       method: "POST",
@@ -42,6 +44,40 @@ signUpForm.addEventListener('submit', async function(event){
     const responseData = await response.json();
     token = responseData.token;
 });
+
+signInForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const email = data.get("email");
+    const password = data.get("password");
+  
+    const response = await fetch(`${baseUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+  
+    const responseData = await response.json();
+    token = responseData.data.token;
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function (){
     
